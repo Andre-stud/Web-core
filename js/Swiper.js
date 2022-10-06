732fc89fe25eea4dcde975368392ca9e8025569a
Andre-stud <andrejzakasovskij@gmail.com>
2022-10-06 20:24:00 +0300

    Block_1.5

---
+++ js/Swiper.js
@@ -0,0 +1,38 @@
+// Swiper
+  const slider = document.querySelector('.swiper');
+  
+  let mySwiper;
+
+function mobileSlider() {
+	if (window.innerWidth < 768 && slider.dataset.mobile == 'false') {
+		mySwiper = new Swiper('.swiper', {
+    
+      loop: true,
+      grabCursor: true,
+    
+      // If we need pagination
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      spaceBetween: 16,
+      freeMode: true,
+      updateOnWindowResize: true,
+      slidesPerView: 'auto',
+    });
+
+		slider.dataset.mobile = 'true';
+	}
+
+	if (window.innerWidth >= 768 ) {
+		slider.dataset.mobile = 'false';
+		if (slider.classList.contains('swiper-initialized')) {
+			mySwiper.destroy();
+		}
+	}
+}
+
+mobileSlider()
+
+window.addEventListener('resize', () => {
+	mobileSlider();
+});
